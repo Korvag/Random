b63eaeeedacfcde8ee01b5ee02999f8cee8c62e1
Korvag <btonerfranciscan@gmail.com>
2023-08-28 11:47:46 -0400

    new files

---
+++ fun.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+app = QApplication([])
+okButton = QPushButton('Ok')
+cancelButton = QPushButton('Cancel')
+test = QLineEdit()
+test.setEchoMode(QLineEdit.Password)
+label = QLabel('Subscribe')
+label.setFont(QFont('Arial',20))
+
+hbox = QHBoxLayout()
+hbox.addStretch(1)
+hbox.addWidget(okButton)
+hbox.addWidget(cancelButton)
+hbox.addWidget(test)
+vbox = QVBoxLayout()
+vbox.addStretch(1)
+vbox.addWidget(label)
+vbox.addLayout(hbox)
+
+win = QWidget()
+win.setLayout(vbox)
+win.setWindowTitle('TESTTEST')
+win.show()
+sys.exit(app.exec_())
+
+
